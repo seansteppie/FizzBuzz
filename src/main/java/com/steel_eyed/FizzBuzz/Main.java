d633a8d244c9e277472fa0eedb96cc06ffcc40bf
Sean Steppie <Sean.Steppie@steel-eyed.com>
2016-06-14 14:05:53 +0100

    Add interative entry of numbers

--- src/main/java/com/steel_eyed/FizzBuzz/Main.java
+++ src/main/java/com/steel_eyed/FizzBuzz/Main.java
@@ -1,14 +1,16 @@
 package com.steel_eyed.FizzBuzz;
 
+import java.util.Scanner;
+
 public class Main {
     /**
      * Prints out to System.out Fizz Buzz values from start to end.
      *
      * @param start int
-     * @param end int
+     * @param end   int
      */
     private static void fizzBuzz( int start, int end ) {
-        for ( int i = start; i <= end; i++ ) {
+        for( int i = start; i <= end; i++ ) {
             System.out.println( isFizzBuzz( i ) );
         }
     }
@@ -30,17 +32,30 @@ public class Main {
      */
     public static String isFizzBuzz( int n ) {
         String output = "";
-        if ( n % 3 == 0 )
+        if( n % 3 == 0 )
             output += "Fizz";
-        if ( n % 5 == 0 )
+        if( n % 5 == 0 )
             output += ( output.equals( "" ) ? "" : " " ) + "Buzz";
-        if ( output.equals( "" ) )
+        if( output.equals( "" ) )
             output += n;
         return output;
     }
 
-
     public static void main( String[] args ) {
-        fizzBuzz( 100 );
+        Scanner in = new Scanner( System.in );
+        int start = 0, end = 0;
+        System.out.print( "Enter either 1 to do one number or 2 to enter a range: " );
+        int selection = in.nextInt();
+        if( selection == 1 ) {
+            System.out.print( "Enter number: " );
+            start = in.nextInt();
+            System.out.println( isFizzBuzz( start ) );
+        } else {
+            System.out.print( "Enter Start number: " );
+            start = in.nextInt();
+            System.out.print( "Enter End number: " );
+            end = in.nextInt();
+            fizzBuzz( start, end );
+        }
     }
 }
